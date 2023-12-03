67e3e2aabcb806fdf6a86d27ee2d7f787e72830f
Vonney <davonnejv@gmail.com>
2023-12-02 21:07:58 -0600

    installing tailwind and setting up header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <> hellooo</>;
+export default function App() {
+  return (
+    <div>
+      <div className="flex flex-row bg-black w-full">
+        <h1 className="text-3xl text-white font-orbitron p-4 ml-40">SpaceZ</h1>;
+      </div>
+    </div>
+  );
 }
-
-export default App;
