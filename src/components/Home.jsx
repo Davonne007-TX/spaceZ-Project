8120a5b669281e29fca62bc974d14597421c180e
Vonney <davonnejv@gmail.com>
2023-12-09 18:53:24 -0600

    adding map function and adding section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const capitalizeLetters = (word) => {
+  return word.toUpperCase();
+};
+
 export default function Home() {
+  const navItems = [
+    "Eagle7",
+    "Eagle Light",
+    "Phoenix10",
+    "Mothership",
+    "Spaceflight",
+    "Mars Mission",
+    "Beyond",
+  ];
+
   return (
     <div
       className="h-screen bg-cover bg-no-repeat"
@@ -10,15 +27,23 @@ export default function Home() {
         </h1>
 
         <nav className="flex mt-5 font-orbitron ml-10 gap-10 text-sm text-white list-none cursor-pointer">
-          <li className="hover:underline">Eagle7</li>
-          <li className="hover:underline">Eagle Light</li>
-          <li className="hover:underline">Phoenix10</li>
-          <li className="hover:underline">Mothership</li>
-          <li className="hover:underline">Spaceflight</li>
-          <li className="hover:underline">Mars Mission</li>
-          <li className="hover:underline">Beyond</li>
+          {navItems.map((item, index) => (
+            <li key={index} className="hover:underline">
+              <Link to={`/${item.toLowerCase()}`}>
+                {capitalizeLetters(item)}
+              </Link>
+            </li>
+          ))}
         </nav>
       </header>
+      <section className="text-white launch ml-40 font-serif flex flex-col">
+        <span className="text-2xl underline">Upcoming Launch</span>
+        <span className="text-5xl mt-2">USSA-76 MISSION</span>
+
+        <button className="border-2 border-white mt-5 p-4 w-32 hover:bg-white hover:text-black">
+          Watch
+        </button>
+      </section>
     </div>
   );
 }
